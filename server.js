1e3d90fd4e52501c5f0cc8e50accfb2cbaab826b
Glitch (hello-express) <none>
2023-11-13 00:27:57 +0000

    🕹😋 Checkpoint ./artists.json:53833513/205 ./server.js:53833513/6404

--- server.js
+++ server.js
@@ -4,31 +4,96 @@
 // init project
 const express = require('express');
 const app = express();
+app.use(express.json());
 
-//load my .json file of english dictionary word
-const dictionary = require('./dictionary.json');
+// load my .json files
+const songs = require('./songs.json');
+const artists = require('./artists.json');
 
-//define your routes here. don't forget about error handling
-app.get('/', function(request, response) {
-  response.json({
-    message: "Please see the README.md for documentation",
-  })
+// Routes for songs
+app.get('/songs', function(request, response) {
+  response.json(songs);
 });
 
-app.get('/dictionary', function(request, response) {
-  response.json(dictionary)
+app.get('/songs/:title', function(request, response) {
+  let title = request.params['title'];
+  if (songs[title]) {
+    response.json(songs[title]);
+  } else {
+    response.status(404).json({ message: "Song not found" });
+  }
 });
 
-app.get('/dictionary/:word', function(request, response){
-  let word = request.params['word'];
-  let result = {"word":word,
-                  "definition":dictionary[word]};
-  
-  response.json(result)
+app.post('/songs', function(request, response) {
+  // Extract song details from request body
+  let newSong = request.body;
+
+  // Add a unique identifier or title for the new song
+  songs[newSong.title] = newSong;
+
+  response.status(201).json({ message: "Song added successfully", song: newSong });
+});
+app.put('/songs/:title', function(request, response) {
+  let title = request.params['title'];
+  if (songs[title]) {
+    // Update song details
+    songs[title] = request.body;
+    response.json({ message: "Song updated successfully", song: songs[title] });
+  } else {
+    response.status(404).json({ message: "Song not found" });
+  }
+});
+app.delete('/songs/:title', function(request, response) {
+  let title = request.params['title'];
+  if (songs[title]) {
+    delete songs[title];
+    response.json({ message: "Song deleted successfully" });
+  } else {
+    response.status(404).json({ message: "Song not found" });
+  }
 });
 
+// Routes for artists
+app.get('/artists', function(request, response) {
+  response.json(artists);
+});
 
+app.get('/artists/:name', function(request, response) {
+  let name = request.params['name'];
+  if (artists[name]) {
+    response.json(artists[name]);
+  } else {
+    response.status(404).json({ message: "Artist not found" });
+  }
+});
+app.post('/artists', function(request, response) {
+  // Extract artist details from request body
+  let newArtist = request.body;
 
+  // Add a unique identifier or name for the new artist
+  artists[newArtist.name] = newArtist;
+
+  response.status(201).json({ message: "Artist added successfully", artist: newArtist });
+});
+app.put('/artists/:name', function(request, response) {
+  let name = request.params['name'];
+  if (artists[name]) {
+    // Update artist details
+    artists[name] = request.body;
+    response.json({ message: "Artist updated successfully", artist: artists[name] });
+  } else {
+    response.status(404).json({ message: "Artist not found" });
+  }
+});
+app.delete('/artists/:name', function(request, response) {
+  let name = request.params['name'];
+  if (artists[name]) {
+    delete artists[name];
+    response.json({ message: "Artist deleted successfully" });
+  } else {
+    response.status(404).json({ message: "Artist not found" });
+  }
+});
 
 
 // listen for requests :)
